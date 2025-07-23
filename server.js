d16e194498f90fc2335d6886a121bfc0185449eb
Nexteach 360 <vivalinguaofc@gmail.com>
2025-07-23 19:28:20 +0300

    package jason.

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const connectDatabase = require("./utils/dataBase");
 connectDatabase();
 
 const port = process.env.PORT || 8000;
-
 app.listen(port, () => {
   console.log(`Server listening on port ${port}...`);
 });
